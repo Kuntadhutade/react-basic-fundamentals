c5b87f8b39baa73a9d0575e5b0bc0d5d33f92f39
Kuntadhutade <kuntadhutade@gmail.com>
2026-03-23 00:14:59 +0530

    added app.jsx file

---
+++ src/components/01-Components.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Components() {
+    
+    return(
+        <div>
+          <h2>This is my first componets</h2>
+          <p>This is a simple functional components</p>
+        </div>
+    )
+}
+
+
+export default Components;
