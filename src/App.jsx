9c7af7bbf17b4717b019005a8d69ef55a03723ee
Kuntadhutade <kuntadhutade@gmail.com>
2026-03-24 12:36:44 +0530

    replaced the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Components from './components/01-Components'
 import Props from './components/02-Props';
+import State from './components/03-state';
+import Event from './components/04-Event';
+import EventHandling from './components/05-EventHandling';
+import Listkeys from './components/06-Listkeys';
+import Listkey from './components/Listkey';
 
 import './App.css'
+
 
 function App() {
   const items = ["Home","About","Services","contact","Join Us"]
@@ -9,8 +15,14 @@ function App() {
 
   return (
     <>
-     <Components/>
-    <Props  logoText="NavbarLogo" color="white" backgroundColor="#030e45" items={items} />
+     {/* <Components/> */}
+    {/* <Props  logoText="NavbarLogo" color="white" backgroundColor="#030e45" items={items} /> */}
+    {/* <State/> */}
+    {/* <Event/> */}
+    {/* <EventHandling /> */}
+    {/* <Listkeys/> */}
+    <Listkey/>
+
     </>
   )
 }
