bc2bd7e9345883e79c6adfcadd4b6abc645bc4cd
Kuntadhutade <kuntadhutade@gmail.com>
2026-04-02 17:32:50 +0530

    updated the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import Components from './components/01-Components'
 import Props from './components/02-Props';
 import State from './components/03-state';
@@ -5,8 +6,15 @@ import Event from './components/04-Event';
 import EventHandling from './components/05-EventHandling';
 import Listkeys from './components/06-Listkeys';
 import Listkey from './components/Listkey';
-
-import './App.css'
+import ConditionalRender from './components/07-ConditionalRender';
+import ConditionRender from './components/ConditionRender';
+import Forms from './components/08-forms';
+import LoginForm from './components/08-LoginForm';
+import About from "./components/09-routing/About";
+import Contact from "./components/09-routing/Contact";
+import Home from "./components/09-routing/Home";
+import Navbar from "./components/09-routing/Navbar"
+import {Routes, Route} from "react-router-dom"
 
 
 function App() {
@@ -14,6 +22,7 @@ function App() {
   
 
   return (
+
     <>
      {/* <Components/> */}
     {/* <Props  logoText="NavbarLogo" color="white" backgroundColor="#030e45" items={items} /> */}
@@ -21,7 +30,21 @@ function App() {
     {/* <Event/> */}
     {/* <EventHandling /> */}
     {/* <Listkeys/> */}
-    <Listkey/>
+    {/* <Listkey/> */}
+    {/* <ConditionalRender/> */}
+    {/* <ConditionRender /> */}
+    {/* <Forms/> */}
+    {/* <LoginForm/> */}
+
+   
+
+    <Navbar/>
+
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
 
     </>
   )
